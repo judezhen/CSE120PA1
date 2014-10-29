5304206d828b9dd5f50ee0bc0fad050eb859341f
Steven Yuan <citrus@citrus>
2014-10-29 16:00:21 -0700

    Part 1 tests 6-7 completed

--- threadtest.cc
+++ threadtest.cc
@@ -1,6 +1,6 @@
 
 // threadtest.cc 
-//	Test cases for the threads assignment.
+//  Test cases for the threads assignment.
 
 #include "copyright.h"
 #include "system.h"
@@ -47,49 +47,8 @@ ThreadTest1()
 }
 
 //----------------------------------------------------------------------
-// part 1 tests
+// BEGIN PART 1 TESTING CODE
 //----------------------------------------------------------------------
-
-Lock *locktest1 = NULL;
-
-void
-LockThread1(int param)
-{
-    printf("L1:0\n");
-    locktest1->Acquire();
-    printf("L1:1\n");
-    currentThread->Yield();
-    printf("L1:2\n");
-    locktest1->Release();
-    printf("L1:3\n");
-}
-
-void
-LockThread2(int param)
-{
-    printf("L2:0\n");
-    locktest1->Acquire();
-    printf("L2:1\n");
-    currentThread->Yield();
-    printf("L2:2\n");
-    locktest1->Release();
-    printf("L2:3\n");
-}
-
-
-void
-LockTest1()
-{
-    DEBUG('t', "Entering LockTest1");
-
-    locktest1 = new Lock("LockTest1");
-
-    Thread *t = new Thread("one");
-    t->Fork(LockThread1, 0);
-    t = new Thread("two");
-    t->Fork(LockThread2, 0);
-}
-
 
 // acquiring the same lock twice
 void testLockTwice()
@@ -114,12 +73,8 @@ void testReleaseUnheldLock() {
 }
 
 // deleting a lock that is held
-// void testDeleteEmptyLock() {
-//     Lock lock = new Lock("test");
-//     lock->Acquire();
-//     delete lock;
-//     printf("\n");
-// }
+void testDeleteEmptyLock() { }
+
 
 //waiting on a condition variable without holding a lock
 void testCondition() {
@@ -129,78 +84,58 @@ void testCondition() {
     printf("Test failed.\n");
 }
 
-/*-----------------------------------------------------
-               cv Sinal test
-______________________________________________________ */
-
 
 /* Signaling a condition variable wakes only one thread 
-   and broadcasting wakes up all threads */
+   and broadcasting wakes up all threads 
+   need to comment on what the successful results look like
+   to prep for writeup
+*/
+
+//SIGNAL TEST ENVIRONMENT WITH TWO THREADS
 Condition *testCondtion_helper = new Condition("cond");
 Lock * lock = new Lock("lock1");;
 
 void testFunction1(int param){
-        printf("testing Function 1 \n");
-        lock->Acquire();
-        testCondtion_helper->Wait(lock);
-        printf("signaled Function 1\n");
-        lock->Release();
+    printf("testing Function 1 \n");
+    lock->Acquire();
+    testCondtion_helper->Wait(lock);
+    printf("signaled Function 1\n");
+    lock->Release();
 }
 void testFunction2(int param){
-        testCondtion_helper->Signal(lock);
-        printf("testing Function 2 \n");
-        printf("signaled Function 2\n");
+    testCondtion_helper->Signal(lock);
+    printf("testing Function 2 \n");
+    printf("signaled Function 2\n");
 }
-
-
 
 void testSignal() {
-      
     Thread *t = new Thread("one");    
     t->Fork(testFunction1, 0);
-
-    Thread *t2 = new Thread("two"); 
-    t2->Fork(testFunction2, 0);
-
-
+    t = new Thread("two"); 
+    t->Fork(testFunction2, 0);
 }
 
 
-/*-----------------------------------------------------
-               cv broadcast test
-______________________________________________________ */
-
-
-/* Signaling a condition variable wakes only one thread 
-   and broadcasting wakes up all threads */
-// Condition *testCondtion_helper = new Condition("cond");
-// Lock * lock = new Lock("lock1");;
-
+//BROADCAST TEST ENVIRONMENT WITH THREE THREADS
 Condition *testCondtion_helper1 = new Condition("cond1");
 Lock * lock1 = new Lock("lock1");;
 
-
 void testFun1(int param){   
     lock1->Acquire();  
-        printf("testing Function 1 \n");
-        testCondtion_helper1->Wait(lock1);
-        
-        printf("broadcast Function 1\n");
-
+    printf("testing Function 1 \n");
+    testCondtion_helper1->Wait(lock1);    
+    printf("broadcast Function 1\n");
 }
 void testFun2(int param){
     lock1->Acquire();  
-         printf("testing Function 2 \n");
-        testCondtion_helper1->Wait(lock1);
-        
-        printf("signaled Function 2\n");
+     printf("testing Function 2 \n");
+    testCondtion_helper1->Wait(lock1);    
+    printf("signaled Function 2\n");
   }
-
 void testFun3 (int param){
-        printf("testing Function 3\n");
-        testCondtion_helper1->Broadcast(lock1);
-     
-        printf("signaled Function 3\n");       
+    printf("testing Function 3\n");
+    testCondtion_helper1->Broadcast(lock1);
+    printf("signaled Function 3\n");       
 }
 void testBroadcast() {
     Thread *t = new Thread("one");  
@@ -212,30 +147,68 @@ void testBroadcast() {
 }
 
 
+/*  Signaling and broadcasting to a condition variable with 
+    no waiters is a no-op, and future threads that wait will block 
+    (i.e., the signal/broadcast is "lost")
+    
+    Explanation of success:
+    Signal and Broadcast methods don't do anything because of how 
+    they're written forks a new thread, acquires a lock, then waits. 
+    Thread is blocked successfully when the print statement after
+    it waits doesn't run.
+ */
+
+Lock *testLock = NULL;
+Condition *testCond = NULL;
+
+void testWait(int param) {
+    testLock->Acquire();
+    testCond->Wait(testLock);
+    printf("Block unsuccessful.");
+    //test that thread "one" is in the queue
+}
+void testNoOp() {
+    testCond = new Condition("testNoOpCV");
+    testLock = new Lock ("testNoOpLock");
+    testCond->Signal(testLock); //check that nothing happened
+    printf("Signal did nothing.\n");
+    testCond->Broadcast(testLock); //check that nothing happened
+    printf("Broadcast did nothing.\n");
+    Thread *t = new Thread("one");
+    t->Fork(testWait,0);
+}
+
+/*  Fail when a thread calling signal holds the lock passed in to signal
+    Will make an assertion fail in Signal because Signal checks if
+    the calling thread holds the lock being passed in.
+*/
+void testSignalHoldLockWait(int param) {
+    printf("Acquiring\n");
+    testLock->Acquire();
+    printf("Signaling\n");
+    testCond->Signal(testLock);
+}
+void testSignalHoldLock() {
+    testCond = new Condition("testSignalHoldLockCV");
+    testLock = new Lock ("testSignalHoldLockLock");
+    Thread *t = new Thread("one");
+    t->Fork(testSignalHoldLockWait,0);
+}
 
 
+/*  deleting a lock or condition variable should have no threads on 
+    the wait queue
+*/
 
+void testDeleteLockCVNoThreads() {}
 
+//----------------------------------------------------------------------
+// END PART 1 TESTING CODE
+//----------------------------------------------------------------------
 
-
-
-// signaling and broadcasting to a condition variable with no waiters is a no-op, 
-// and future threads that wait will block (i.e., the signal/broadcast is "lost")
-// void testNoOp() {
-
-// }
-// /*a thread calling signal holds the lock passed in to signal*/
-// void testSignalHoldLock() {
-
-// }
-// /* deleting a lock or condition variable should have no threads on the wait queue*/
-// void testDeleteLockCVNoThreads() {
-
-// }
 
 //------------------------------------------------------------------------
-//MailboxTest
-//
+// BEGIN PART 2 TESTING CODE
 //------------------------------------------------------------------------
 // Mailbox *Mailboxtest = NULL;
 
@@ -249,10 +222,27 @@ void testBroadcast() {
 //     printf("what is in the mailbox : %d \n ", in);
 
 //     delete Mailboxtest;
-
-
 // }
 
+//------------------------------------------------------------------------
+// END PART 2 TESTING CODE
+//------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------
+// BEGIN PART 3 TESTING CODE
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+// END PART 3 TESTING CODE
+//------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------
+// BEGIN PART 4 TESTING CODE
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+// END PART 4 TESTING CODE
+//------------------------------------------------------------------------
 
 
 //----------------------------------------------------------------------
@@ -263,23 +253,15 @@ void
 ThreadTest()
 {
     switch (testnum) {
-   // case 1:  ThreadTest1();  break;
-    //case 2:  LockTest2();    break;
-    //case 3:  LockTest3();    break;
-    case 4:   testLockTwice(); break;
-    case 5:   testReleaseUnheldLock(); break;
-    case 6:   testCondition(); break;
-    case 7:   testSignal(); break;
-    case 8:   testBroadcast(); break;
+    case 1: testLockTwice(); break;
+    case 2: testReleaseUnheldLock(); break;
+    case 3: testCondition(); break;
+    case 4: testSignal(); break;
+    case 5: testBroadcast(); break;
+    case 6: testNoOp(); break;
+    case 7: testSignalHoldLock(); break;
     default: 
 	printf("No test specified.\n");
 	break;
     }
 }
-
-
-
-
-
-
-
